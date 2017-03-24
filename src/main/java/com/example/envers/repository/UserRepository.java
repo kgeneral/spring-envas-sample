d5ea13403ce43386a69d734c21dae448382c8248
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2017-03-24 15:30:09 +0900

    remove QueryDslPredicateExecutor temporary

--- src/main/java/com/example/envers/repository/UserRepository.java
+++ src/main/java/com/example/envers/repository/UserRepository.java
@@ -2,8 +2,7 @@ package com.example.envers.repository;
 
 import com.example.envers.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.history.RevisionRepository;
 
-public interface UserRepository extends JpaRepository<User, String>, QueryDslPredicateExecutor<User>, RevisionRepository<User, String, Integer> {
+public interface UserRepository extends JpaRepository<User, String>, RevisionRepository<User, String, Integer> {
 }
